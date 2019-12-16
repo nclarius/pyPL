508b95f10de42496b57789efd247fdb8677b4ff6
Natalie Clarius <zitronenbaum@users.noreply.github.com>
2019-12-16 17:57:48 +0100

    Update PL.py

--- PL.py
+++ PL.py
@@ -4,7 +4,7 @@ Works only on models with finite domains (obviously) and languages with a finite
 © Natalie Clarius <natalie.clarius@student.uni-tuebingen.de>
 
 Known issues:
-- No interactive mode; source code as to be edited to set up examples.
+- No interactive mode; source code has to be edited in order to set up input.
 - Entering formulas is cumbersome.
 - Entering singleton tuples is cumbersome.
 - indent has to be reset manually after each evaluation.
