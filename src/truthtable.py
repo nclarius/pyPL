da6afeded5dac2a1d7b48d6c4e667d29abcd54c6
Natalie Clarius <natalie_clarius@yahoo.de>
2024-08-12 23:59:20 +0200

    fix plain text truth table output

--- src/truthtable.py
+++ src/truthtable.py
@@ -14,56 +14,54 @@ from itertools import product
 
 class Truthtable():
 
-    def __init__(self, e: Expr, premises=[], latex=True, silent=False, gui=None):
-        self.e = e
+    def __init__(self, fml: Formula, premises=[], latex=True, silent=False, gui=None):
+        self.fml = fml
         self.prems = premises
-        self.inf = Inf(self.e, self.prems) if e else Neg(Inf(self.e, self.prems))
+        self.inf = Inf(self.fml, self.prems) if fml else Neg(Inf(self.fml, self.prems))
         self.latex = latex
         self.silent = silent
         self.gui = gui
         if not self.gui:
             self.gui = __import__("gui").PyPLGUI(True)
         
+        self.pvs = sorted(list((self.fml.propvars() if self.fml else set()).union(*[p.propvars() for p in self.prems])))
+        self.vprod = list(product([True, False], repeat=len(self.pvs)))
+        self.valuations = [{p: v for (p, v) in zip(self.pvs, valuation)} for valuation in self.vprod]
+        
         if not self.silent:
             self.show()
 
     def truthtable(self):
-        pvs = sorted(list((self.e.propvars() if self.e else set()).union(*[p.propvars() for p in self.prems])))
-        vprod = list(product([True, False], repeat=len(pvs)))
-        valuations = [{p: v for (p, v) in zip(pvs, valuation)} for valuation in vprod]
-        syms = [c for c in " ".join([f.tex() for f in self.prems + [self.inf] + ([self.e] if self.e else [])]).split(" ")]
-        parens = [c for c in " ".join([f.tex() for f in self.prems + [self.inf] + ([self.e] if self.e else [])]) if c in ["(", ")"]]
-
         if not self.latex:
             tt = ""
             # heading
-            tt += ((len(str(len(valuations))) + 2) * " ") + " ".join(pvs) + " | "
+            tt += ((len(str(len(self.valuations))) + 2) * " ") + " ".join(self.pvs) + (" | " if self.prems else "")
             tt += " | ".join([str(p).replace("¬", "¬ ") for p in self.prems])
-            tt += " | " + str(Inf()) + " | "
-            tt += str(self.e).replace("¬", "¬ ") + "\n"
+            tt += " | " + str(self.inf) + (" | " if self.fml else " ")
+            tt += (str(self.fml).replace("¬", "¬ ") if self.fml else "") + "\n"
             # line
-            tt += ((len(str(len(valuations))) + 2) * "-") + (2 * len(pvs)) * "-" + "|-"
-            tt += "-|-".join([self.truthrowsep(p, True) for p in self.prems])
-            tt += "-|" + self.truthrowsep(Inf(), True) + "|-"
-            tt += self.truthrowsep(fml, True) + "\n"
+            tt += ((len(str(len(self.valuations))) + 2) * "-") + (2 * len(self.pvs)) * "-" + "|" + ("-" if self.prems else "")
+            tt += "-|-".join([self.truthrowsep(p, True) for p in self.prems]) + ("-|" if self.prems else "")
+            tt += self.truthrowsep(self.inf, True)
+            tt += ("|-" + self.truthrowsep(self.fml, True) if self.fml else "") + "\n"
             # rows
             tt += "\n".join(["V" + str(i+1) + " "+ \
-                             " ".join([self.truthvalue(b) for b in val.values()]) + " | " + \
+                             " ".join([self.truthvalue(b) for b in val.values()]) + (" | " if self.prems else "") + \
                             " | ".join([self.truthrow(p, val, True) for p in self.prems]) + \
-                            " | " + self.truthrow(Inf(), val, True) + " | " + \
-                             self.truthrow(self.e, val, True)
-                             for i, val in enumerate(valuations)])
+                            " | " + self.truthrow(self.inf, val, True) + (" | " if self.fml else "") + \
+                            (self.truthrow(self.fml, val, True) if self.fml else "")
+                             for i, val in enumerate(self.valuations)])
         else:
             tt = ""
-            tt += "\\begin{tabular}{c" + len(pvs) * "c" + "|"
+            tt += "\\begin{tabular}{c" + len(self.pvs) * "c" + "|"
             tt += "|".join([(len(f.tex().split(" ")) + len([c for c in f.tex() if c in ["(", ")"]])) * "c" for f in 
-                self.prems + [self.inf] + ([self.e] if self.e else [])]) + "}\n"
+                self.prems + [self.inf] + ([self.fml] if self.fml else [])]) + "}\n"
             # todo decrease spacing between parentheses columns
             # heading
-            tt += " & " + " & ".join("$" + p + "$" for p in pvs) + " & " + \
+            tt += " & " + " & ".join("$" + p + "$" for p in self.pvs) + " & " + \
                   (" & ".join(["$" + c + "$" for c in 
                     " ".join([f.tex() for f in 
-                        self.prems + [self.inf] + ([self.e] if self.e else [])])
+                        self.prems + [self.inf] + ([self.fml] if self.fml else [])])
                         .split(" ")]))\
                    .replace("(", "($ & $").replace(")", "$ & $)") + \
                   "\\\\ \\hline\n"
@@ -71,15 +69,15 @@ class Truthtable():
             tt += "\\\\\n".join(["$V_{" + str(i+1) + "}$ & " +
                                  " & ".join([self.truthvalue(b) for b in val.values()]) + " & " +
                                  " & ".join([self.truthrow(p, val, True) for p in self.prems]) + (" & " if self.prems else "") +
-                                 self.truthrow(self.inf, val, True) + (" & " if self.e else "") +
-                                 (self.truthrow(self.e, val, True) if self.e else "")
-                                 for i, val in enumerate(valuations)])
+                                 self.truthrow(self.inf, val, True) + (" & " if self.fml else "") +
+                                 (self.truthrow(self.fml, val, True) if self.fml else "")
+                                 for i, val in enumerate(self.valuations)])
             tt += "\\\\\n" + "\\end{tabular}"
         return tt
     
     def truthrowsep(self, e, mainconn=False):
         if not self.latex:
-            if isinstance(e, Inf):
+            if isinstance(e, Inf) or isinstance(e, Neg) and isinstance(e.phi, Inf):
                 return "-" + ("=" if mainconn else "-") + "-"
             if hasattr(e, "phi"):
                 if hasattr(e, "psi"):
@@ -174,12 +172,23 @@ class Truthtable():
             heading = "Truth table for "
             heading += ", ".join([str(p) for p in self.prems])
             heading += " " + str(self.inf) + " "
-            heading += str(self.e) + ":\n\n"
+            heading += str(self.fml) + ":\n\n"
         else:
             heading = "Truth table for $"
             heading += ", ".join([p.tex() for p in self.prems])
             heading += " " + self.inf.tex() + " "
-            heading += (self.e.tex() if self.e else "") + "$:\\\\ \\ \\\\ \n"
+            heading += (self.fml.tex() if self.fml else "") + "$:\\\\ \\ \\\\ \n"
+        
+        # result
+        subj = ("sentence" if not self.prems else ("inference" if self.fml else "theory"))
+        match subj:
+            case "sentence":
+                prop = "valid" if self.valid() else "contingent" if self.satisfiable() else "unsatisfiable"
+            case "inference":
+                prop = "valid" if self.valid() else "invalid"
+            case "theory":
+                prop = "satisfiable" if self.satisfiable() else "unsatisfiable"
+        result = "The " + subj + " is " + prop + "."
 
         # load preamble
         if self.latex:
@@ -190,30 +199,37 @@ class Truthtable():
 
         # assemble string
         if not self.latex:
-            res = heading + tt + ("\n\n" + comptime if comptime else "") + "\n"
+            res = heading + tt + "\n\n" + result + ("\n\n" + comptime if comptime else "") + "\n"
         else:
             res = preamble + \
                   "\n\n\\begin{document}\n\n" + \
                   heading + \
                   tt + \
-                  ("\\\\ \\ \\\\ \\ \\\\ \n" + comptime if comptime else "") +\
+                  "\\\\ \\ \\\\ \\ \\\\ \n" + result +\
+                  ("\\\\ \\ \\\\ \n" + comptime if comptime else "") +\
                   "\n\n\\end{document}"
 
         # write and open output
         self.gui.write_output(res, self.latex)
+    
+    def valid(self):
+        return all([self.inf.denot(PropStructure("S", v)) for v in self.valuations])
+    
+    def satisfiable(self):
+        return any([self.inf.denot(PropStructure("S", v)) for v in self.valuations])
 
 if __name__ == "__main__":
     pass
     parse_f = __import__("parser").FmlParser().parse
 
     fml = parse_f("((p & q) | ~ r)")
-    tt = Truthtable(fml, silent=True)
+    tt = Truthtable(fml, latex=False)
 
     fml1 = parse_f("p -> q")
     fml2 = parse_f("~ p")
     fml = parse_f("~ q")
-    tt = Truthtable(fml, premises=[fml1, fml2], silent=True)
+    tt = Truthtable(fml, premises=[fml1, fml2], latex=False)
 
     fml1 = parse_f("p v q")
     fml2 = parse_f("~ (p & q)")
-    tt = Truthtable(None, premises=[fml1, fml2], silent=True)
+    tt = Truthtable(None, premises=[fml1, fml2], latex=False)
