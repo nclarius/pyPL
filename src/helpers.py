430403b574e4cd0d3afe36e6b5176ec98110d94b
Natalie Clarius <natalie_clarius@yahoo.de>
2025-08-20 15:11:54 +0200

    release sleep inhibition when program terminated

--- src/helpers.py
+++ src/helpers.py
@@ -1,5 +1,6 @@
 
 import os
+import signal
 import dbus
 
 class SleepInhibitor:
@@ -8,32 +9,55 @@ class SleepInhibitor:
         self.who = "pyPL"
         self.why = reason
         self.mode = "block"
-        self.file_descriptor = None
-        self.file_descriptor_file = None
+        self.fd = None  # file descriptor
+        self.fd_file = None  # file descriptor file
+        self.sig_handlers = {}  # original signal handlers
 
     def __enter__(self):
         try:
+            # check that systemd is available
             with open("/proc/1/comm", "r") as f:
                 if "systemd" not in f.read().strip():
                     raise Exception("systemd not available")
 
+            # set up dbus connection
             bus = dbus.SystemBus()
             proxy = bus.get_object('org.freedesktop.login1', '/org/freedesktop/login1')
             iface = dbus.Interface(proxy, 'org.freedesktop.login1.Manager')
 
-            self.file_descriptor = iface.Inhibit(
+            # inhibit
+            self.fd = iface.Inhibit(
                 self.what,
                 self.who,
                 self.why,
                 self.mode,
                 byte_arrays=True
             )
-            self.file_descriptor_file = os.fdopen(self.file_descriptor.take(), 'rb')
-            return self
+            self.fd_file = os.fdopen(self.fd.take(), 'rb')
+
+            # save and override signal handlers
+            for sig in (signal.SIGINT, signal.SIGTERM, signal.SIGTSTP, signal.SIGHUP):
+                self.sig_handlers[sig] = signal.getsignal(sig)
+                signal.signal(sig, self._cleanup)
+
+        except Exception as e:
+            print("Failed to inhibit sleep:", e)
         
-        except Exception:
-            return self
+        return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
-        if self.file_descriptor_file:
-            self.file_descriptor_file.close()
+        self._cleanup()
+    
+    def _cleanup(self, signum=None, frame=None):
+        # close descriptor file
+        if self.fd_file:
+            self.fd_file.close()
+            self.fd_file = None
+
+        # restore original signal handlers
+        for sig, handler in self.sig_handlers.items():
+            signal.signal(sig, handler)
+
+        # re-raise signal with default handler
+        if signum:
+            os.kill(os.getpid(), signum)
